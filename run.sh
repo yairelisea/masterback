c14e0e2bb94abf0145651f0b264bba9447ee7dee
Iwebs <44825570+yairelisea@users.noreply.github.com>
2025-10-26 11:10:59 -0600

    changes run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -e
+
+# Corre las migraciones de la base de datos
+echo "Corriendo migraciones de la base de datos..."
+alembic upgrade head
+echo "Migraciones completadas."
+
+# Inicia la aplicación
 export $(grep -v '^#' .env | xargs) || true
 uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
