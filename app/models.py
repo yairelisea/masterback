44e9ed6c99d651a59c96ade0edd1f88b9c24d8ff
Iwebs <44825570+yairelisea@users.noreply.github.com>
2025-11-01 21:00:07 -0600

    fixedmodels

--- app/models.py
+++ app/models.py
@@ -29,7 +29,7 @@ from sqlalchemy import (
 from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
 
-Base = declarative_base()
+Base = DeclarativeBase()
 
 # ------------------------
 # User
